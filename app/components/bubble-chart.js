98f7c18f88c9127207e4286fd2ec9b69112beec4
Jan Klinger <jan.klinger@typedflow.com>
2023-03-21 17:14:09 +0100

    VIP: create a working bubble chart

--- app/components/bubble-chart.js
+++ app/components/bubble-chart.js
@@ -11,6 +11,11 @@ export default class BubbleChartComponent extends Component {
 
   @action
   async createChart(element) {
+    // Initialize an array to hold the data for each hour and day of the week
+    const data = Array(24)
+      .fill()
+      .map(() => Array(7).fill(0));
+
     const chartData = {
       datasets: [
         {
@@ -28,11 +33,41 @@ export default class BubbleChartComponent extends Component {
         x: {
           type: 'linear',
           position: 'bottom',
+          min: -1,
+          max: 24,
+          ticks: {
+            stepSize: 1,
+            callback: (value) => {
+              if (value >= 0 && value <= 23) {
+                return `${value}:00`;
+              } else {
+                return '';
+              }
+            },
+          },
         },
         y: {
           type: 'linear',
           position: 'left',
+          min: -1,
+          max: 7,
+          ticks: {
+            values: [0, 1, 2, 3, 4, 5, 6],
+            callback: (value) =>
+              [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+              ][value],
+          },
         },
+      },
+      layouts: {
+        padding: 10,
       },
     };
 
@@ -44,13 +79,25 @@ export default class BubbleChartComponent extends Component {
 
     // Update chart data every 5 seconds
     this.interval = setInterval(() => {
-      const socialPosts = this.upfluenceStream.socialPosts;
-      this.chart.data.datasets[0].data = socialPosts.map((post) => {
-        return {
-          x: post.timestamp,
-          y: post.timestamp,
-          r: 10,
-        };
+      const socialPosts = this.upfluenceStream.socialPosts; // get updated social posts
+
+      // Iterate over the socialPosts and increment the appropriate element in the newData array
+      socialPosts.forEach((post) => {
+        const date = new Date(post.timestamp * 1000);
+        const hour = date.getHours();
+        const day = date.getDay();
+        data[hour][day]++;
+      });
+
+      chartData.datasets[0].data = [];
+      data.forEach((hourData, hour) => {
+        hourData.forEach((count, day) => {
+          chartData.datasets[0].data.push({
+            x: hour,
+            y: day,
+            r: count,
+          });
+        });
       });
       this.chart.update();
     }, 5000);
