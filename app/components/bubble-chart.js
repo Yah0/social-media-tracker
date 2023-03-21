7bd4f3e45faf245a9e070e77e29a1cf78414349e
Jan Klinger <jan.klinger@typedflow.com>
2023-03-21 15:08:42 +0100

    make update of chart works but with incorrectly named axis

--- app/components/bubble-chart.js
+++ app/components/bubble-chart.js
@@ -1,19 +1,21 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import Chart from 'chart.js/auto';
+import { inject as service } from '@ember/service';
 
 export default class BubbleChartComponent extends Component {
+  @service upfluenceStream;
+
+  chart = null;
+  interval = null;
+
   @action
-  createChart(element) {
-    const data = {
+  async createChart(element) {
+    const chartData = {
       datasets: [
         {
           label: 'Commit Activity by Day and Hour',
-          data: [
-            { x: 10, y: 20, r: 10 },
-            { x: 15, y: 10, r: 20 },
-            { x: 25, y: 15, r: 30 },
-          ],
+          data: [],
           backgroundColor: 'rgba(255, 99, 132, 0.2)',
           borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1,
@@ -34,10 +36,29 @@ export default class BubbleChartComponent extends Component {
       },
     };
 
-    const chart = new Chart(element, {
+    this.chart = new Chart(element, {
       type: 'bubble',
-      data: data,
+      data: chartData,
       options: options,
     });
+
+    // Update chart data every 5 seconds
+    this.interval = setInterval(() => {
+      const socialPosts = this.upfluenceStream.socialPosts;
+      this.chart.data.datasets[0].data = socialPosts.map((post) => {
+        return {
+          x: post.timestamp,
+          y: post.timestamp,
+          r: 10,
+        };
+      });
+      this.chart.update();
+    }, 5000);
+  }
+
+  willDestroy() {
+    // Clear the interval when the component is destroyed
+    super.willDestroy(...arguments);
+    clearInterval(this.interval);
   }
 }
