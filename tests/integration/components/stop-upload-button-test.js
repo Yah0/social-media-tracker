ca5a361364c8d78824555f05904cd2623fe362f5
Jan Klinger <jan.klinger@typedflow.com>
2023-03-26 14:36:21 +0200

    fix text expectation on stop-upload-button test

--- tests/integration/components/stop-upload-button-test.js
+++ tests/integration/components/stop-upload-button-test.js
@@ -27,6 +27,6 @@ module('Integration | Component | StopUploadButton', function (hooks) {
     assert.ok(true, 'handleClick was called');
 
     // Check that the button text is correct
-    assert.dom('button').hasText('Data Stream Stopped');
+    assert.dom('button').hasText('Start Data Stream');
   });
 });
